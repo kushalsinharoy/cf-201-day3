817d46d6a509c6b38bbca76f0f026eaa56edbffa
Kushal Sinha Roy <kushalr@amazon.com>
2019-02-24 18:32:52 -0800

    Use Functions for the Guessing Game

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ var correctAnswers = 0;
 if (firstName || lastName) {
     //Validating user first name and last name
     nameValidation(firstName, lastName);
+    
     //Check if user wants to play the game
     var guessingGameFlag = prompt("Ready to play a guesssing game about me?");
 
@@ -25,42 +26,35 @@ if (firstName || lastName) {
         alert("Lets Rock and Roll!!!!!");
 
         // 1st Function
-        var questionOneTwo = functionQuestionOneTwo();
-        var numberOfQuestionOneTwo = questionOneTwo[0];
-        var correctAnswersOneTwo = questionOneTwo[1];
+        var questionHomeCountry = getMyHomeCountry();
+        var numberOfQuestionHomeCountry = questionHomeCountry[0];
+        var correctAnswersHomeCountry = questionHomeCountry[1];
 
-        //increment the counter 
-        numberOfQuestion = numberOfQuestion + numberOfQuestionOneTwo;
-        correctAnswers = correctAnswers + correctAnswersOneTwo;
+        //increment the counter with function returns
+        numberOfQuestion = numberOfQuestion + numberOfQuestionHomeCountry;
+        correctAnswers = correctAnswers + correctAnswersHomeCountry;
+        console.log('Number of Question ' + numberOfQuestion + ' Number of correct answer ' + correctAnswers);
 
 
         //3rd Question
-        var favoritePastTime = prompt('Is playing video games my favorite past time?');
-        numberOfQuestion++;
-        console.log('Favorite Past Time', favoritePastTime);
+        var questionPastTime = getMyFavoritePastTime();
+        var numberOfQuestionPastTime = questionPastTime[0];
+        var correctAnswersPastTime = questionPastTime[1];
 
-        if (favoritePastTime.toUpperCase() === 'YES' || favoritePastTime.toUpperCase() === 'Y') {
-            correctAnswers++;
-            alert("Correct Answer!!!");
-
-            //4th Question
-            var favoriteVideoGame = prompt('Is FIFA my favorite video game?');
-            numberOfQuestion++;
-            console.log('Favorite video game', favoriteVideoGame);
-
-            if (favoriteVideoGame.toUpperCase() === 'Y' || favoriteVideoGame.toUpperCase() === 'YES') {
-                correctAnswers++;
-                alert("Correct Answer!!!");
-            }
-        }
+        //increment the counter with function returns
+        numberOfQuestion = numberOfQuestion + numberOfQuestionPastTime;
+        correctAnswers = correctAnswers + correctAnswersPastTime;
+        console.log('Number of Question ' + numberOfQuestion + ' Number of correct answer ' + correctAnswers);
 
         //5th Question
         var favoriteHobby = prompt('Is photography one of my favorite hobby?');
         numberOfQuestion++;
         console.log('Favorite Hobby', favoriteHobby);
         if (favoriteHobby.toUpperCase() === 'Y' || favoriteHobby.toUpperCase() === 'YES') {
+            correctAnswers++;
             alert("Correct Answer!!!");
         }
+        console.log('Number of Question ' + numberOfQuestion + ' Number of correct answer ' + correctAnswers);
 
         alert("Ok. You have another 3 chances to guess my favorite number. HINT: It's a multiple of 3");
         numberOfQuestion++;
@@ -83,34 +77,24 @@ if (firstName || lastName) {
             }
             attempts++;
         }
+        console.log('Number of Question ' + numberOfQuestion + ' Number of correct answer ' + correctAnswers);
 
         //7th Question
         alert("Ok. Can you guess 3 of my favorite places to visit in my bucket list. HINT: 90% of them are in Europe");
         numberOfQuestion++;
-        var getFavoritePlacesFromUser = 0;
-        var favoritePlaceUserArray = [];
+
+        // declare the favorite places array
         var favoritePlaces = ["BARCELONA", "VENICE", "ZURICH", "ISTANBUL", "AMSTERDAM", "SANTORINI"];
         var correctAnswersFavoritePlaces = 0;
 
-        while (getFavoritePlacesFromUser <= 2) {
-            favoritePlaceUserArray.push(prompt('Enter favorite place  ' + (getFavoritePlacesFromUser + 1)));
-            console.log(favoritePlaceUserArray[getFavoritePlacesFromUser]);
-            getFavoritePlacesFromUser++;
-        }
-
-        //loop through the favorite places and check answer
-        for (var i = 0; i < favoritePlaceUserArray.length; i++) {
-            for (var j = 0; j <= favoritePlaces.length; j++) {
-                if (favoritePlaceUserArray[i].toUpperCase() === favoritePlaces[j]) {
-                    correctAnswersFavoritePlaces++;
-                }
-            }
-        }
+        //Function call for validating favorite places
+        correctAnswersFavoritePlaces = getMyFavoritePlaces(favoritePlaces);
 
         //increment total correct answers id user has provided all 3 correctly
         if (correctAnswersFavoritePlaces === 3) {
             alert("BINGO!!! You got all the 3 correct")
             correctAnswers++;
+            console.log('Number of Question ' + numberOfQuestion + ' Number of correct answer ' + correctAnswers);
         } else {
             alert("You got " + correctAnswersFavoritePlaces + " favorite places correct out of 3 correct!!!");
         }
@@ -118,24 +102,22 @@ if (firstName || lastName) {
     } else {
         alert("Thanks and appreciate your time searching for this guessing game!!!");
     }
-} else{
+} else {
     alert("Thanks for visiting my home page, Enjoy the day");
 }
 
 /* First Name and Last Name Validations for alerting welcome */
-
-function nameValidation(firstName, lastName){
-    firstName= firstName.toUpperCase()
+function nameValidation(firstName, lastName) {
+    firstName = firstName.toUpperCase()
     lastName = lastName.toUpperCase()
-    
-   if((!!firstName) && (!!lastName)){
+
+    if ((!!firstName) && (!!lastName)) {
         alert('Greetings ' + firstName + ' ' + lastName + '!!! ' + 'Welcome to my small guessing game!!!!');
     } else {
-        if (!!lastName){
+        if (!!lastName) {
             alert('Greetings ' + lastName + '!!! ' + 'Welcome to my small guessing game!!!!');
         }
-        if (!!firstName)
-        {
+        if (!!firstName) {
             alert('Greetings ' + firstName + ' ' + '!!! ' + 'Welcome to my small guessing game!!!!');
         }
     }
@@ -143,7 +125,7 @@ function nameValidation(firstName, lastName){
 
 // Function for Question one and two
 // Returns the number of questions and correct answers as array
-function functionQuestionOneTwo() {
+function getMyHomeCountry() {
     var numberOfQuestion = 0;
     var correctAnswers = 0;
 
@@ -168,4 +150,56 @@ function functionQuestionOneTwo() {
         }
     }
     return [numberOfQuestion, correctAnswers];
+}
+
+// Function for Question 3 & 4
+// Returns the number of questions and correct answers as array
+function getMyFavoritePastTime() {
+    var numberOfQuestion = 0;
+    var correctAnswers = 0;
+
+    //3rd Question
+    var favoritePastTime = prompt('Is playing video games my favorite past time?');
+    numberOfQuestion++;
+    console.log('Favorite Past Time', favoritePastTime);
+
+    if (favoritePastTime.toUpperCase() === 'YES' || favoritePastTime.toUpperCase() === 'Y') {
+        correctAnswers++;
+        alert("Correct Answer!!!");
+
+        //4th Question
+        var favoriteVideoGame = prompt('Is FIFA my favorite video game?');
+        numberOfQuestion++;
+        console.log('Favorite video game', favoriteVideoGame);
+
+        if (favoriteVideoGame.toUpperCase() === 'Y' || favoriteVideoGame.toUpperCase() === 'YES') {
+            correctAnswers++;
+            alert("Correct Answer!!!");
+        }
+    }
+    return [numberOfQuestion, correctAnswers];
+}
+
+//Function for Question 7
+// Array of string is passed, and an integer is returned
+function getMyFavoritePlaces(favoritePlaces) {
+    var correctAnswersFavoritePlaces = 0;
+    var getFavoritePlacesFromUser = 0;
+    var favoritePlaceUserArray = [];
+
+    while (getFavoritePlacesFromUser <= 2) {
+        favoritePlaceUserArray.push(prompt('Enter favorite place  ' + (getFavoritePlacesFromUser + 1)));
+        console.log(favoritePlaceUserArray[getFavoritePlacesFromUser]);
+        getFavoritePlacesFromUser++;
+    }
+
+    //loop through the favorite places and check answer
+    for (var i = 0; i < favoritePlaceUserArray.length; i++) {
+        for (var j = 0; j <= favoritePlaces.length; j++) {
+            if (favoritePlaceUserArray[i].toUpperCase() === favoritePlaces[j]) {
+                correctAnswersFavoritePlaces++;
+            }
+        }
+    }
+    return correctAnswersFavoritePlaces;
 }
